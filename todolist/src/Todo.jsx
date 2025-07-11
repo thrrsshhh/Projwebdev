e4fa2ab5825f89be2fdf5610775b30e2c1d85165
thrrsshhh <Thrishashetty5112004@gmail.com>
2025-07-11 17:27:41 +0530

    todolist modified

--- todolist/src/Todo.jsx
+++ todolist/src/Todo.jsx
@@ -2,51 +2,106 @@ import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 export default function TodoList() {
-  let [todos, setTodos] = useState([
-    { task: "", id: uuidv4() }
-  ]);
-  let [newTodo, setNewTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
 
-  let addNewTask = () => {
-    setTodos([...todos, { task: newTodo, id: uuidv4() }]);
+  // Add a new task
+  const addNewTask = () => {
+    if (newTodo.trim() === "") return;
+    setTodos([...todos, { id: uuidv4(), task: newTodo, isCompleted: false, isEditing: false }]);
     setNewTodo("");
   };
 
-
-  let updateTodoValue = (event) => {
-    setNewTodo(event.target.value);
+  // Delete a task
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
   };
-  let deleteTodo = (id) => {
-  setTodos((prevTodos) => todos.filter((prevTodos) => prevTodos.id !== id));
-};
+
+  // Toggle complete/incomplete
+  const toggleComplete = (id) => {
+    setTodos(todos.map((todo) =>
+      todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+    ));
+  };
+
+  // Start editing
+  const startEdit = (id) => {
+    setTodos(todos.map((todo) =>
+      todo.id === id ? { ...todo, isEditing: true } : todo
+    ));
+  };
+
+  // Save edited task
+  const saveEdit = (id, updatedTask) => {
+    setTodos(todos.map((todo) =>
+      todo.id === id ? { ...todo, task: updatedTask, isEditing: false } : todo
+    ));
+  };
+
+  // Cancel editing
+  const cancelEdit = (id) => {
+    setTodos(todos.map((todo) =>
+      todo.id === id ? { ...todo, isEditing: false } : todo
+    ));
+  };
+
+  // Separate tasks
+  const pendingTasks = todos.filter((todo) => !todo.isCompleted);
+  const completedTasks = todos.filter((todo) => todo.isCompleted);
 
   return (
-    <div>
+    <div style={{ fontFamily: "Arial", padding: "20px" }}>
+      <h2>To-Do App</h2>
       <input
-        placeholder="add a task"
+        placeholder="Add a task"
         value={newTodo}
-        onChange={updateTodoValue}
+        onChange={(e) => setNewTodo(e.target.value)}
       />
-      <br /><br />
       <button onClick={addNewTask}>Add Task</button>
 
-      <br /><br /><br /><br />
+      <br /><br />
       <hr />
-<h4>Tasks Todo</h4>
-<hr />
-<ul>
-  {
-  todos.map((todo) => (
-    <li key={todo.id}>
-      <span>{todo.task}</span>
-      &nbsp;&nbsp;&nbsp;
-      <button onClick={() => deleteTodo(todo.id)}>delete</button>
-      
-    </li>
-  ))
-}
+      <h4>Pending Tasks</h4>
+      <hr />
+      <ul>
+        {pendingTasks.map((todo) => (
+          <li key={todo.id}>
+            {todo.isEditing ? (
+              <>
+                <input
+                  type="text"
+                  defaultValue={todo.task}
+                  onChange={(e) => todo.task = e.target.value}
+                />
+                <button onClick={() => saveEdit(todo.id, todo.task)}>Save</button>
+                <button onClick={() => cancelEdit(todo.id)}>Cancel</button>
+              </>
+            ) : (
+              <>
+                <span>{todo.task}</span>
+                &nbsp;&nbsp;
+                <button onClick={() => toggleComplete(todo.id)}>Mark Done</button>
+                <button onClick={() => startEdit(todo.id)}>Edit</button>
+                <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+              </>
+            )}
+          </li>
+        ))}
+      </ul>
 
-</ul>
+      <hr />
+      <h4>Completed Tasks</h4>
+      <hr />
+      <ul>
+        {completedTasks.map((todo) => (
+          <li key={todo.id}>
+            <span style={{ textDecoration: "line-through" }}>{todo.task}</span>
+            &nbsp;&nbsp;
+            <button onClick={() => toggleComplete(todo.id)}>Mark Incomplete</button>
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
